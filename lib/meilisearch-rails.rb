0ef630964f9a41a2bdc192ad92e738df72f7fc30
CaroFG <carolina.ferreira131@gmail.com>
2021-03-16 18:00:29 +0100

    Add MeiliSearch settings

--- lib/meilisearch-rails.rb
+++ lib/meilisearch-rails.rb
@@ -68,39 +68,10 @@ module MeiliSearch
 
     # MeiliSearch settings
     OPTIONS = [
-      # Attributes
-      :searchableAttributes, :attributesForFaceting, :unretrievableAttributes, :attributesToRetrieve,
-      :attributesToIndex, #Legacy name of searchableAttributes
-      # Ranking
-      :ranking, :customRanking, # Replicas are handled via `add_replica`
-      # Faceting
-      :maxValuesPerFacet, :sortFacetValuesBy,
-      # Highlighting / Snippeting
-      :attributesToHighlight, :attributesToSnippet, :highlightPreTag, :highlightPostTag,
-      :snippetEllipsisText, :restrictHighlightAndSnippetArrays,
-      # Pagination
-      :hitsPerPage, :paginationLimitedTo,
-      # Typo
-      :minWordSizefor1Typo, :minWordSizefor2Typos, :typoTolerance, :allowTyposOnNumericTokens,
-      :disableTypoToleranceOnAttributes, :disableTypoToleranceOnWords, :separatorsToIndex,
-      # Language
-      :ignorePlurals, :removeStopWords, :camelCaseAttributes, :decompoundedAttributes,
-      :keepDiacriticsOnCharacters, :queryLanguages, :indexLanguages,
-      # Query Rules
-      :enableRules,
-      # Query Strategy
-      :queryType, :removeWordsIfNoResults, :advancedSyntax, :optionalWords,
-      :disablePrefixOnAttributes, :disableExactOnAttributes, :exactOnSingleWordQuery, :alternativesAsExact,
-      # Performance
-      :numericAttributesForFiltering, :allowCompressionOfIntegerArray,
-      :numericAttributesToIndex, # Legacy name of numericAttributesForFiltering
-      # Advanced
-      :attributeForDistinct, :distinct, :replaceSynonymsInHighlight, :minProximity, :responseFields,
-      :maxFacetHits,
-
-      # Rails-specific
-      :synonyms, :placeholders, :altCorrections,
+      :searchableAttributes, :attributesForFaceting, :displayedAttributes, :distinctAttribute,
+      :synonyms, :stopWords, :rankingRules
     ]
+
     OPTIONS.each do |k|
       define_method k do |v|
         instance_variable_set("@#{k}", v)
