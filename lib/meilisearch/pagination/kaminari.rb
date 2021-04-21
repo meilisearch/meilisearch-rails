61b5ac3a320cbd04c854e1f474fc11f6e8ea2cbd
CaroFG <carolina.ferreira131@gmail.com>
2021-04-21 19:22:41 +0200

    Remove unnecessary param that caused pagination issue

--- lib/meilisearch/pagination/kaminari.rb
+++ lib/meilisearch/pagination/kaminari.rb
@@ -25,12 +25,12 @@ module MeiliSearch
       class << self
         def create(results, total_hits, options = {})
           offset = ((options[:page] - 1) * options[:per_page])
-          array = new results, :offset => offset, :limit => options[:per_page], :total_count => total_hits
+          array = new results, :limit => options[:per_page], :offset => offset
           if array.empty? and !results.empty?
             # since Kaminari 0.16.0, you need to pad the results with nil values so it matches the offset param
             # otherwise you'll get an empty array: https://github.com/amatsuda/kaminari/commit/29fdcfa8865f2021f710adaedb41b7a7b081e34d
             results = ([nil] * offset) + results
-            array = new results, :offset => offset, :limit => options[:per_page], :total_count => total_hits
+            array = new results, :offset => offset, :limit => options[:per_page]
           end
           array
         end
