7da3269bed933ecbcef7762b32e36fa92154eefd
CaroFG <carolina.ferreira131@gmail.com>
2021-04-21 12:34:01 +0200

    Add placeholder search

--- playground/app/controllers/search_controller.rb
+++ playground/app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
     def index
         @q = params['book'] && params['book']['q']
-        return if @q.blank?
+        @hits = Book.search('') if @q.blank?
 
         @hits = Book.search(@q, { hitsPerPage: 5, page: (params['page'] || 1) })
     end    
