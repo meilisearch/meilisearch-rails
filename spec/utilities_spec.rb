b0542d207a5b758500bcee45e3d54017e7372a46
Bruno Casali <brunoocasali@gmail.com>
2022-01-28 17:40:27 -0300

    Add smoke tests to rake tasks
    Related to https://github.com/meilisearch/meilisearch-rails/issues/110

--- spec/utilities_spec.rb
+++ spec/utilities_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rake'
 
 describe MeiliSearch::Rails::Utilities do
   around do |example|
@@ -12,5 +13,27 @@ describe MeiliSearch::Rails::Utilities do
 
   it 'gets the models where Meilisearch module was included' do
     expect(described_class.get_model_classes - [Dummy, DummyChild, DummyGrandChild]).to be_empty
+  end
+
+  context 'when invoked from rake task' do
+    before do
+      file = File.join('..', 'lib', 'meilisearch', 'rails', 'tasks', 'meilisearch')
+      Rake.application.rake_require file.to_s
+      Rake::Task.define_task(:environment)
+    end
+
+    {
+      reindex: :reindex_all_models,
+      set_all_settings: :set_settings_all_models,
+      clear_indexes: :clear_all_indexes
+    }.each do |task, method_name|
+      it "calls #{described_class}.#{method_name} successfully" do
+        allow(described_class).to receive(method_name).and_return(nil)
+
+        Rake.application.invoke_task "meilisearch:#{task}"
+
+        expect(described_class).to have_received(method_name)
+      end
+    end
   end
 end
