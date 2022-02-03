2869c18c3522b78f8a91dbd2a1974f673995fad4
cjilbert504 <54157657+cjilbert504@users.noreply.github.com>
2022-02-03 15:16:54 -0600

    Update spec/integration_spec.rb
    Co-authored-by: Bruno Casali <brunoocasali@gmail.com>

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -835,7 +835,9 @@ describe 'Colors' do
     blue = Color.create!(name: 'blue', short_name: 'blu', hex: 0x0000FF)
     black = Color.create!(name: 'black', short_name: 'bla', hex: 0x000000)
     green = Color.create!(name: 'green', short_name: 'gre', hex: 0x00FF00)
+
     facets = Color.search('*', { sort: ['name:asc'] })
+
     expect(facets).to eq([black, blue, green])
   end
 end
