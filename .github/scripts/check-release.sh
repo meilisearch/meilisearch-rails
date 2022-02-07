b66d8ea09bf2b1bca44306b61a176ccbdb05de80
Bruno Casali <brunoocasali@gmail.com>
2022-02-07 19:45:56 -0300

    Fix release version checker

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -2,7 +2,7 @@
 
 # Checking if current tag matches the package version
 current_tag=$(echo $GITHUB_REF | tr -d 'refs/tags/v')
-file_tag=$(grep 'VERSION = ' lib/meilisearch/version.rb | cut -d '=' -f 2- | tr -d ' ' | tr -d \')
+file_tag=$(grep 'VERSION = ' lib/meilisearch/rails/version.rb | cut -d '=' -f 2- | tr -d ' ' | tr -d \')
 if [ "$current_tag" != "$file_tag" ]; then
   echo "Error: the current tag does not match the version in package file(s)."
   echo "$current_tag vs $file_tag"
