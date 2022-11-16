348ad7fd5fdc03c6a82947ea5155dd130879ca1d
Bruno Casali <brunoocasali@gmail.com>
2022-11-16 19:23:02 -0300

    Remove fixed limit of 200 itens when searching with page

--- lib/meilisearch/rails/pagination/kaminari.rb
+++ lib/meilisearch/rails/pagination/kaminari.rb
@@ -35,7 +35,7 @@ module MeiliSearch
             if array.empty? && !results.empty?
               # since Kaminari 0.16.0, you need to pad the results with nil values so it matches the offset param
               # otherwise you'll get an empty array: https://github.com/amatsuda/kaminari/commit/29fdcfa8865f2021f710adaedb41b7a7b081e34d
-              results = ([nil] * offset) + results
+              results = Array.new(offset) + results
               array = new results, offset: offset, limit: options[:per_page], total_count: total_hits
             end
 
