9732c0a8e1617af969402529d2e4c7b936d8b014
Anderson Peligrini <apeligrini@gmail.com>
2023-03-17 18:18:08 -0300

    Fix rails cache serialization (marshal dump)

--- lib/meilisearch-rails.rb
+++ lib/meilisearch-rails.rb
@@ -59,7 +59,6 @@ module MeiliSearch
         attributesToHighlight
         attributesToCrop
         cropLength
-        faceting
         pagination
         faceting
         typoTolerance
@@ -599,13 +598,6 @@ module MeiliSearch
       end
 
       module AdditionalMethods
-        def self.extended(base)
-          class << base
-            alias_method :raw_answer, :ms_raw_answer unless method_defined? :raw_answer
-            alias_method :facets_distribution, :ms_facets_distribution unless method_defined? :facets_distribution
-          end
-        end
-
         def ms_raw_answer
           @ms_json
         end
@@ -613,6 +605,9 @@ module MeiliSearch
         def ms_facets_distribution
           @ms_json['facetDistribution']
         end
+
+        alias raw_answer ms_raw_answer unless method_defined? :raw_answer
+        alias facets_distribution ms_facets_distribution unless method_defined? :facets_distribution
 
         private
 
