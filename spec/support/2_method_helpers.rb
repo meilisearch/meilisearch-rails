083d1e2da205acf6c2d95c0e7b9500bbcb2629f4
ellnix <103502144+ellnix@users.noreply.github.com>
2023-11-29 11:17:05 +0100

    Fix indexes name conflict in specs

--- spec/support/2_method_helpers.rb
+++ spec/support/2_method_helpers.rb
@@ -1,16 +1,16 @@
 # A unique prefix for your test run in local or CI
 SAFE_INDEX_PREFIX = "rails_#{SecureRandom.hex(8)}".freeze
 
-def indexes
+def _indexes
   @indexes ||= {}
 end
 
 # avoid concurrent access to the same index in local or CI
 def safe_index_uid(name)
-  indexes[name] ||= "#{SAFE_INDEX_PREFIX}_#{name}"
+  _indexes[name] ||= "#{SAFE_INDEX_PREFIX}_#{name}"
 end
 
 # get a list of safe indexes in local or CI
 def safe_index_list
-  indexes.values.flat_map { |safe_idx| [safe_idx, "#{safe_idx}_test"] }
+  _indexes.values.flat_map { |safe_idx| [safe_idx, "#{safe_idx}_test"] }
 end
