315df3a401df44a8e5be0b0958145b784a22f898
ellnix <103502144+ellnix@users.noreply.github.com>
2024-02-28 15:58:05 +0100

    Split spec Sequel models into their own files

---
+++ spec/support/sequel_db.rb
@@ -0,0 +1,13 @@
+def sequel_db
+  @sequel_db = Sequel.connect(if defined?(JRUBY_VERSION)
+                                'jdbc:sqlite:sequel_data.sqlite3'
+                              else
+                                { 'adapter' => 'sqlite',
+                                  'database' => 'sequel_data.sqlite3' }
+                              end)
+end
+
+FileUtils.rm('sequel_data.sqlite3') if File.exist?('sequel_data.sqlite3')
+
+Dir["#{File.dirname(__FILE__)}/sequel_models/*.rb"].sort.each { |file| require file }
+
---
+++ spec/support/sequel_models/book.rb
@@ -0,0 +1,41 @@
+require 'support/sequel_db'
+
+sequel_db.create_table(:sequel_books) do
+  primary_key :id
+  String :name
+  String :author
+  FalseClass :released
+  FalseClass :premium
+end
+
+class SequelBook < Sequel::Model(sequel_db)
+  plugin :active_model
+
+  include MeiliSearch::Rails
+
+  meilisearch synchronous: true, index_uid: safe_index_uid('SequelBook'), sanitize: true do
+    add_attribute :test
+    add_attribute :test2
+
+    searchable_attributes [:name]
+  end
+
+  def after_create
+    SequelBook.new
+  end
+
+  def test
+    'test'
+  end
+
+  def test2
+    'test2'
+  end
+
+  private
+
+  def public?
+    released && !premium
+  end
+end
+
