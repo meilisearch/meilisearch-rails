26e607c98d58a6fe6cf9467dc122140925fbd820
ellnix <103502144+ellnix@users.noreply.github.com>
2024-02-29 19:30:07 +0100

    Move Post specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -112,28 +112,6 @@ describe 'NestedItem' do
   end
 end
 
-describe 'Posts' do
-  before(:all) do
-    Post.clear_index!(true)
-  end
-
-  it 'eagerly loads associations' do
-    post1 = Post.new(title: 'foo')
-    post1.comments << Comment.new(body: 'one')
-    post1.comments << Comment.new(body: 'two')
-    post1.save!
-
-    post2 = Post.new(title: 'bar')
-    post2.comments << Comment.new(body: 'three')
-    post2.comments << Comment.new(body: 'four')
-    post2.save!
-
-    assert_queries(2) do
-      Post.reindex!
-    end
-  end
-end
-
 describe 'Colors' do
   before do
     Color.clear_index!(true)
