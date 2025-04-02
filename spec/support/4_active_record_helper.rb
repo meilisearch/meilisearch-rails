ab19423ce1b6a26a5c995a6f9d9abbe7bc9e08ec
ellnix <ellnix@disroot.org>
2025-04-02 15:14:25 +0200

    Decouple test models from ActiveRecord
    Intention is to allow testing multiple ORMs more easily, with equivalent
    data.

---
+++ spec/support/4_active_record_helper.rb
@@ -0,0 +1,45 @@
+# This file is responsible for one-time set up of ActiveRecord
+# 1 - Set ActiveRecord connection & options
+# 2 - Delete existing database
+# 3 - Expose "TestModels::ActiveRecord.initialize_model" to allow dynamically creating models
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new($stdout)
+ActiveRecord::Base.logger.level = Logger::WARN
+ActiveRecord::Base.establish_connection(
+  'adapter' => defined?(JRUBY_VERSION) ? 'jdbcsqlite3' : 'sqlite3',
+  'database' => 'data.sqlite3',
+  'pool' => 5,
+  'timeout' => 5000
+)
+
+ActiveRecord::Base.raise_in_transactional_callbacks = true if ActiveRecord::Base.respond_to?(:raise_in_transactional_callbacks)
+
+FileUtils.rm_f('data.sqlite3')
+
+module Models
+  module ActiveRecord
+    def self.schema
+      @schema ||= ::ActiveRecord::Schema.new.tap { |schema| schema.verbose = false }
+    end
+
+    def self.initialize_model(specification)
+      schema.create_table specification.name.underscore.pluralize do |t|
+        specification.fields.each do |field|
+          t.send(field.type, field.name)
+        end
+      end
+
+      klass = Class.new(::ActiveRecord::Base) do
+        define_singleton_method(:model_name) do
+          name = "Models::ActiveRecord::#{specification.name}"
+          ActiveModel::Name.new(self, nil, name)
+        end
+      end
+
+      const_set(specification.name, klass)
+
+      klass.class_eval(&specification.body)
+    end
+  end
+end
