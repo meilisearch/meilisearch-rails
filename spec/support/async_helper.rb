e9aef702c43b8b817f0f2aa9f2704aaea0a60c3a
Strift <lau.cazanove@gmail.com>
2026-06-09 16:08:27 +0800

    Update helper to scope task queries

--- spec/support/async_helper.rb
+++ spec/support/async_helper.rb
@@ -1,7 +1,7 @@
 module AsyncHelper
   def self.task_cursor(index_uids:, types: nil)
     ensure_index_uids!(index_uids)
-    options = { limit: 1 }
+    options = { limit: 1, index_uids: task_index_uids(index_uids) }
     options[:types] = types if types
 
     tasks = Meilisearch::Rails.client.tasks(options)['results']
@@ -11,7 +11,7 @@ module AsyncHelper
 
   def self.wait_for_pending_tasks(index_uids:, after_uid: nil, types: nil, timeout: 5, interval: 0.01)
     ensure_index_uids!(index_uids)
-    options = { statuses: %w[enqueued processing], limit: 1000 }
+    options = { statuses: %w[enqueued processing], limit: 1000, index_uids: task_index_uids(index_uids) }
     options[:types] = types if types
 
     Timeout.timeout(timeout) do
@@ -46,5 +46,22 @@ module AsyncHelper
     return if index_uids.is_a?(Array) && index_uids.any? { |uid| !uid.nil? && !uid.to_s.empty? }
 
     raise ArgumentError, '`index_uids` must contain at least one non-empty index uid'
+  end
+
+  def self.task_index_uids(index_uids)
+    environment_suffix = index_uid_environment_suffix
+    return index_uids unless environment_suffix
+
+    index_uids.map do |uid|
+      uid = uid.to_s
+      uid.end_with?(environment_suffix) ? uid : "#{uid}#{environment_suffix}"
+    end
+  end
+
+  def self.index_uid_environment_suffix
+    return unless Meilisearch::Rails.configuration[:per_environment]
+    return unless defined?(Rails) && Rails.respond_to?(:env)
+
+    "_#{Rails.env}"
   end
 end
